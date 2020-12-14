44383c667839d9696c86d2c49b32335da5afd941
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-12-14 17:59:22 +0100

    allowed host

--- greensourceconsult/settings.py
+++ greensourceconsult/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1' , '.greensourcenergy.com', '18.224.234.12']
 
 
 # Application definition
