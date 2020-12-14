866b00976f3fd1ff41a220d01b16ca62468bfef9
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-12-14 21:21:53 +0100

    chaged debug to false

--- greensourceconsult/settings.py
+++ greensourceconsult/settings.py
@@ -31,9 +31,9 @@ load_dotenv(dotenv_path)
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1' , '.greensourcenergy.com', '18.224.234.12']
+ALLOWED_HOSTS = ['127.0.0.1' , '.greensourcenergy.com']
 
 
 # Application definition
